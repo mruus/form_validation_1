aa2743c2124859028bfc044f052d6a5ba0251203
Mansuur Abdullahi Abdirahman <mansuurtech101@gmail.com>
2021-10-15 21:49:07 +0300

    All changed have been applied

---
+++ app.js
@@ -0,0 +1,66 @@
+function checkInput(id) {
+  document.getElementById(id).addEventListener("change", function () {
+    let labelEl = this.parentNode.children[1];
+    if (this.value == "") {
+      this.style.borderColor = "red";
+      this.style.color = "red";
+      labelEl.style.color = "red";
+      labelEl.style.top = "50%";
+      return false;
+    }
+    this.style.borderColor = "blue";
+    this.style.color = "blue";
+    labelEl.style.color = "blue";
+    labelEl.style.top = "0";
+    labelEl.style.padding = "0px 10px";
+    return true;
+  });
+}
+
+function checkEmail(id) {
+  document.getElementById(id).addEventListener("change", function () {
+    let labelEl = this.parentNode.children[1];
+    if (this.value == "") {
+      this.style.borderColor = "red";
+      this.style.color = "red";
+      labelEl.style.color = "red";
+      labelEl.style.top = "50%";
+      return false;
+    }
+
+    if (!this.value.includes("@gmail.com")) {
+      this.style.borderColor = "red";
+      this.style.color = "red";
+      labelEl.style.color = "red";
+      labelEl.style.top = "0";
+      labelEl.style.padding = "0px 10px";
+      return false;
+    }
+    this.style.borderColor = "blue";
+    this.style.color = "blue";
+    labelEl.style.color = "blue";
+    labelEl.style.top = "0";
+    labelEl.style.padding = "0px 10px";
+    return true;
+  });
+}
+
+checkInput("name");
+checkEmail("email");
+checkInput("username");
+checkInput("password");
+
+document.querySelector(".btn").addEventListener("click", clickButton);
+
+function clickButton() {
+  if (
+    !checkInput("name") ||
+    !checkEmail("email") ||
+    !checkInput("username") ||
+    !checkInput("password")
+  ) {
+    alert("Please Check Your Fields, And Make Them Good");
+  } else {
+    alert("Data Is Validated! Good Job");
+  }
+}
